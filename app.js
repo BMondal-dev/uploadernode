8e1b84158b44650b89a02ce8c513eacbdb2e294d
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-26 10:49:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-var express = require('express');
+var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
-var app = express();
-
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/ind.html');
-});
-
-app.post('/', function (req, res){
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
-
-    form.parse(req);
-
-    form.on('fileBegin', function (name, file){
-        file.path = __dirname + '/uploads/' + file.name;
-    });
-
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-    });
-
-    res.sendFile(__dirname + '/ind.html');
-    res.write('Uploaded');
-    res.end();
-});
-
-app.listen(3000);
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080); 
